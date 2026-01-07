6b5cc9c670d12c516e6a6339b47b6aa391a2488b
Armit Soni <armitsoni21@gmail.com>
2026-01-08 04:17:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,126 @@
+let boxes = document.querySelectorAll(".box")
+
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg");
+let turnO = true;    // playerX,playerO
+
+// let arr = ["apple","banana","litchi"];
+
+// // 2D Array
+// let arr2 = [["apple","litchi"],["potato","mushroom"],["pants","shirts"]];
+// console.log(arr2);
+// console.log(arr2[0]);
+// console.log(arr2[0][1]);
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if(turnO){ // player0 turn
+            box.innerText="O";
+            turnO = false;
+        }
+        else{ // turnX // player X turn 
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled = true;  // ye krne s ab jo filled h use dobara fill ni kr skte
+    
+        checkWinner();
+    
+    })
+});
+
+
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+
+ const showWinner=(winner)=>{
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    // disableBoxes();
+ };
+ 
+ const showDraw=()=>{
+    msg.innerText = "It's a Draw";
+    msgContainer.classList.remove("hide");
+    // disableBoxes();
+ };
+
+const checkWinner = ()=>{
+     let isWinner = false;
+    for(let pattern of winPatterns){
+        //  console.log(pattern);
+        //  console.log(pattern[0],pattern[1],pattern[2]);
+        //  console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
+
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
+         if(pos1Val===pos2Val && pos2Val===pos3Val){
+             console.log("winner",pos1Val);
+             showWinner(pos1Val);  // our pos1val going to be our winner
+            disableBoxes();
+            isWinner = true;
+            return; // stop checking further
+         }
+    }
+      // draw check (runs only if no winner)
+       let allFilled = true;
+     for (let box of boxes) {
+      if (box.innerText === "") {
+      allFilled = false;
+      break;
+     }
+     }
+
+ if (!isWinner && allFilled) {
+    msg.innerText = "Game Draw";
+    msgContainer.classList.remove("hide");
+  }
+};
+    
+};
+
+const enableBoxes = ()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText=""; // ab khali krdo sbko ,taki reset kr sku
+    }
+}
+
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+ resetBtn.addEventListener("click", (resetGame))
+ newGameBtn.addEventListener("click",(resetGame))  
+   
+
+
+
+ 
